0346439f0be331350b362df6a1b17022c8b84d89
Wisdom Okogho <wisdommokoghoo@gmail.com>
2024-10-04 08:15:11 +0100

    pages done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 export default function App() {
-  return <div className="bg-red-700">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
