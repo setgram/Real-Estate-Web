88a244b0b1151753938940498262b3ca6de2b658
Wisdom Okogho <wisdommokoghoo@gmail.com>
2024-11-02 23:23:10 +0100

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(
+    "mongodb+srv://setgram:Set%40%23%24080@cluster0.yibil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
